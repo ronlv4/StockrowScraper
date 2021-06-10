632ab5a1791b2612f34514916012cb1f7edf19cd
ronlv4 <ronlv4@gmail.com>
2021-06-10 21:29:13 +0300

    intial commit

--- webscraper/main.py
+++ webscraper/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uvicorn
 from typing import Optional
 
 app = FastAPI()
@@ -9,7 +9,14 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get('/about')
+def about():
+    return "Hello\nThis is a webscraper"
 
 @app.get('/{name}')
 def greeting(name):
     return f'Hello {name}'
+
+
+if __name__ == '__main__':
+    uvicorn.run(app,port=9000)  # for debug purposes
